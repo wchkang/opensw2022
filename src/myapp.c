6e08861c6eadae6ce58711b4015474947ab2ddd4
Woochul Kang <woochul.kang@gmail.com>
2022-11-03 17:54:25 +0900

    some comments are added

--- src/myapp.c
+++ src/myapp.c
@@ -2,11 +2,13 @@
 
 int main()
 {
+   // call functions 3 times...
    for (int i=0; i<3; i++) {
       foo();
       goo();
    }
 
+   // call zoo three times...
    for (int i=0; i<3; i++) {
       zoo();
    }
